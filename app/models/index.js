7dacaf8d4e0501d9af2f957f121cdeb814b0020f
Ewan Sheldon <ewansheldon@gmail.com>
2016-10-25 16:21:46 +0100

    migration

--- app/models/index.js
+++ app/models/index.js
@@ -5,11 +5,17 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require('./config/config.json')[env];
+var config    = require('../../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true
+    });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
