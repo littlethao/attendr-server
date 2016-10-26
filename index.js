d1b68249677f247c631f6a09d700c78f2815fd0e
TomStuart92 <Thomascstuart@gmail.com>
2016-10-26 11:45:03 +0100

    Heroku Fix

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ this.server = http.createServer(function(req, res) {
       res.writeHead(200, {'Content-Type': 'JSON'});
       res.end();
     }
-    else if (req.url === '/events' && req.method == 'GET') {
-      models.Event.findAll().then(function(results){
-        var json = results.map(function(element){ return element.dataValues});
-        res.writeHead(200, {'Content-Type': 'JSON'});
-        res.end(JSON.stringify(json));
-      })
-    }
+  else if (req.url === '/events' && req.method == 'GET') {
+    models.Event.findAll().then(function(results){
+      var json = results.map(function(element){ return element.dataValues});
+      res.writeHead(200, {'Content-Type': 'JSON'});
+      res.end(JSON.stringify(json));
+    })
+  }
+  else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 exports.listen = function() {
