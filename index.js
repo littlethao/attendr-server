060d96adf4c06e6ed12de88e099860c04da88c32
TomStuart92 <Thomascstuart@gmail.com>
2016-10-26 11:35:17 +0100

    Build GET ROOT

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var http = require('http');
 var request = require('request');
+var models = require('./models/index');
 
 this.server = http.createServer(function(req, res) {
   if (req.url === '/') {
       res.writeHead(200, {'Content-Type': 'JSON'});
       res.end();
+    }
+    else if (req.url === '/events' && req.method == 'GET') {
+      models.Event.findAll().then(function(results){
+        var json = results.map(function(element){ return element.dataValues});
+        res.writeHead(200, {'Content-Type': 'JSON'});
+        res.end(JSON.stringify(json));
+      })
     }
 });
 
