2938b904b38add8f6c5adfcd10f0eac713e2bde9
TomStuart92 <Thomascstuart@gmail.com>
2016-10-27 19:11:15 +0100

    MADE IT WORK!

---
+++ spec/features/getEventsSpec.js
@@ -0,0 +1,30 @@
+process.env.NODE_ENV ='test';
+var models = require('../../models/index');
+var request = require('request');
+var server = require('../../index.js')
+
+describe('Event', function() {
+  beforeEach(function(done) {
+    server.listen(3000);
+    models.Event.destroy({where: {name: "Single Mingle"} }).then(function(){
+      models.Event.create({
+          name: "Single Mingle",
+          description: "A Mingle for Singles",
+          address: "52 Commercial St",
+          date:  "2016-10-26 18:30:00+01",
+          link: "http://www.makersacademy.com",
+          meetup_id: "1234"}).then(function() { done() });
+    });
+  });
+
+  afterEach(function() {
+    server.close();
+  });
+
+  it('tests',function(done){
+    request('http://localhost:3000/events', function(error, response, body) {
+      expect(body).toContain('"meetup_id":"1234","name":"Single Mingle"')
+      done()
+    });
+  });
+});
